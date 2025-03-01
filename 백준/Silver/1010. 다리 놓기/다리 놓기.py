ac71f6590862dc94bb782d861628ea49508ae496
LEE JIMIN <80612109+minbarry-0915@users.noreply.github.com>
2025-03-01 20:37:59 +0900

    [Silver V] Title: 다리 놓기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+dp = [[0] * 30 for _ in range(30)]
+
+for i in range(30):
+    for j in range(30):
+        if i == 1:
+            dp[i][j] = j
+        else:
+            if i == j:
+                dp[i][j] = 1
+            elif i < j:
+                dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1]
+
+for _ in range(T):
+    n, m = list(map(int, input().split()))
+    print(dp[n][m])
