d683c586f4f4a0ff70a11f278e6685dda542d16f
LEE JIMIN <80612109+minbarry-0915@users.noreply.github.com>
2025-04-06 21:33:32 +0900

    [Gold IV] Title: 연구소, Time: 2240 ms, Memory: 36092 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,8 +1,9 @@
-import sys
+# 벽 3개 세우고, bfs로 바이러스 퍼트리고, 안전지역 갯수 세고
 import copy
-from collections import deque
+import sys
 
 input = sys.stdin.readline
+from collections import deque
 
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
@@ -12,10 +13,10 @@ dy = [-1, 1, 0, 0]
 
 empty = [(i, j) for i in range(n) for j in range(m) if graph[i][j] == 0]
 virus = [(i, j) for i in range(n) for j in range(m) if graph[i][j] == 2]
-
 answer = 0
 
 
+# 0: 빈칸, 1: 벽, 2: 바이러스
 def bfs():
     tmp_graph = copy.deepcopy(graph)
     queue = deque(virus)
@@ -25,6 +26,7 @@ def bfs():
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
+
             if 0 <= nx < n and 0 <= ny < m:
                 if tmp_graph[nx][ny] == 0:
                     tmp_graph[nx][ny] = 2
@@ -35,17 +37,17 @@ def bfs():
     answer = max(answer, section)
 
 
-def dfs_wall(start, count):
-    if count == 3:
+def makewall(start,cnt):
+    if cnt == 3:
         bfs()
         return
 
     for i in range(start, len(empty)):
         x, y = empty[i]
         graph[x][y] = 1
-        dfs_wall(i + 1, count + 1)
+        makewall(i + 1, cnt + 1)
         graph[x][y] = 0
 
 
-dfs_wall(0, 0)
+makewall(0,0)
 print(answer)
