fb5b28a4a4350c0e598fdeefdab7c98e3d1b9e4f
LEE JIMIN <80612109+minbarry-0915@users.noreply.github.com>
2025-04-14 18:42:55 +0900

    [level 2] Title: 피로도, Time: 29.60 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,18 +1,17 @@
+from itertools import permutations
+
 def solution(k, dungeons):
-    n = len(dungeons)
-    visited = [0] * n
     max_cnt = -1
+    n = len(dungeons)
 
-    def dfs(tired, cnt):
-        nonlocal max_cnt
+    for perm in permutations(dungeons, n):
+        tired = k
+        cnt = 0
+        for need, use in perm:
+            if tired >= need:
+                tired -= use
+                cnt+=1
+            else:
+                break
         max_cnt = max(max_cnt, cnt)
-
-        for i in range(n):
-            if not visited[i]:
-                need, use = dungeons[i]
-                if tired >= need:
-                    visited[i] = True
-                    dfs(tired - use, cnt + 1)
-                    visited[i] = False
-    dfs(k, 0)
     return max_cnt
