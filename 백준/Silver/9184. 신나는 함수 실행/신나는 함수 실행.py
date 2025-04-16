0ad43b9fbd15765b63853b5733a3794b58c9cd1f
LEE JIMIN <80612109+minbarry-0915@users.noreply.github.com>
2025-04-16 20:16:04 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 588 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
@@ -0,0 +1,25 @@
+import sys
+
+sys.setrecursionlimit(10 ** 6)
+
+def w(a, b, c):
+    if a <= 0 or b <= 0 or c <= 0:
+        return 1
+    elif a > 20 or b > 20 or c > 20:
+        return w(20, 20, 20)
+    elif arr[a][b][c]:
+        return arr[a][b][c]
+    elif a < b < c:
+        arr[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c)
+        return arr[a][b][c]
+
+    arr[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1)
+    return arr[a][b][c]
+
+
+arr = [[[0] * 21 for _ in range(21)] for _ in range(21)]
+while True:
+    a, b, c = map(int, input().split())
+    if a == -1 and b == -1 and c == -1:
+        break
+    print(f'w({a}, {b}, {c}) = {w(a, b, c)}')
