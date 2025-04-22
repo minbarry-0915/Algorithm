7f2e629b38d772e7443c7e89d34636ee4ee4b9fb
LEE JIMIN <80612109+minbarry-0915@users.noreply.github.com>
2025-04-22 20:06:33 +0900

    [Silver III] Title: Four Squares, Time: 128 ms, Memory: 111092 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -0,0 +1,13 @@
+import sys
+#sys.stdin = open('input.txt','r')
+input = sys.stdin.readline
+
+n = int(input())
+dp = [i for i in range(0, n + 1)]
+
+import math
+
+for i in range(1, n + 1):
+    for j in range(1, int(math.sqrt(i)) + 1):
+        dp[i] = min(dp[i], dp[i - j * j] + 1)
+print(dp[n])
