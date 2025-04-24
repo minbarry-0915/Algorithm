3b9bad7387428cc714b0749646d3db7bf7065d4f
LEE JIMIN <80612109+minbarry-0915@users.noreply.github.com>
2025-04-24 16:19:52 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 456 ms, Memory: 71968 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
@@ -1,18 +1,14 @@
 import sys
+#sys.stdin = open('input.txt','r')
 input = sys.stdin.readline
 
-MOD = 1000000009
-MAX = 1000000
+dp = [0] * (1000000 + 1)
 
-dp = [0] * (MAX + 1)
 dp[1] = 1
 dp[2] = 2
 dp[3] = 4
-
-# 미리 dp 채우기 (1번만)
-for i in range(4, MAX + 1):
-    dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % MOD
-
+for i in range(4, 1000001):
+    dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1000000009
 t = int(input())
 for _ in range(t):
     n = int(input())
