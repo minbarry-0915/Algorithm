c7ebf1c63af21018e0981b8eda0f5cb3d76f53a4
LEE JIMIN <80612109+minbarry-0915@users.noreply.github.com>
2025-04-30 17:15:37 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder2, Time: 108 ms, Memory: 60,672 KB -BaekjoonHub

--- SWEA/D4/1211. ［S／W 문제해결 기본］ 2일차 － Ladder2/［S／W 문제해결 기본］ 2일차 － Ladder2.py
+++ SWEA/D4/1211. ［S／W 문제해결 기본］ 2일차 － Ladder2/［S／W 문제해결 기본］ 2일차 － Ladder2.py
@@ -1,39 +1,31 @@
+def simulate(i,grid):
+    dist = 1
+    x = 0
+    y = i
+    while x != 99:
+        if y > 0 and grid[x][y - 1] == 1:
+            while y > 0 and grid[x][y - 1] == 1:
+                y -= 1
+                dist += 1
+        elif y < n - 1 and grid[x][y + 1] == 1:
+            while y < n - 1 and grid[x][y + 1] == 1:
+                y += 1
+                dist += 1
+        x += 1
+        dist += 1
+    return dist
 
+n = 100
+for _ in range(10):
+    t = int(input())
+    grid = [list(map(int, input().split())) for _ in range(n)]
 
-N = 100
-T = 10
-
-for _ in range(T):
-    t_num = int(input())
-    grid = [list(map(int, input().split())) for _ in range(N)]
-
-    min_dist = int(1e9)
     answer = 0
-
-    for start_y in range(N):
-        if grid[0][start_y] == 1:
-            x, y = 0, start_y
-            cnt = 0
-            visited = [[False] * N for _ in range(N)]
-            while x < N - 1:
-                visited[x][y] = True
-                # 왼쪽
-                if y > 0 and grid[x][y - 1] == 1 and not visited[x][y - 1]:
-                    while y > 0 and grid[x][y - 1] == 1:
-                        y -= 1
-                        cnt += 1
-                        visited[x][y] = True
-                # 오른쪽
-                elif y < N - 1 and grid[x][y + 1] == 1 and not visited[x][y + 1]:
-                    while y < N - 1 and grid[x][y + 1] == 1:
-                        y += 1
-                        cnt += 1
-                        visited[x][y] = True
-                # 아래로
-                x += 1
-                cnt += 1
-            if cnt < min_dist:
-                min_dist = cnt
-                answer = start_y
-
-    print(f"#{t_num} {answer}")
+    min_dist = int(1e9)
+    for i in range(100):
+        if grid[0][i] == 1:
+            dist = simulate(i,grid)
+            if min_dist >= dist:
+                min_dist = dist
+                answer = i
+    print(f'#{t} {answer}')
