1edb24d8fca90c99e3868d56c7d142e8cd1d27d8
LEE JIMIN <80612109+minbarry-0915@users.noreply.github.com>
2025-05-21 17:51:51 +0900

    [Platinum V] Title: 백조의 호수, Time: 3904 ms, Memory: 136696 KB -BaekjoonHub

--- 백준/Platinum/3197. 백조의 호수/백조의 호수.py
+++ 백준/Platinum/3197. 백조의 호수/백조의 호수.py
@@ -1,51 +1,22 @@
-import sys
-
-# sys.stdin = open('input.txt', 'r', encoding='UTF-8')
-
-input = sys.stdin.readline
-from collections import deque
-
-n, m = map(int, input().split())
-grid = [list(input().strip()) for _ in range(n)]
-
+# 이동 방향 (상하좌우)
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-water_q = deque()
-next_water_q = deque()
-
-swans = []
-for i in range(n):
-    for j in range(m):
-        if grid[i][j] == '.' or grid[i][j] == 'L':
-            water_q.append((i, j))
-            if grid[i][j] == 'L':
-                swans.append((i, j))
-
-days = 0
-swan_q = deque()
-next_swan_q = deque() # 얼음 옆에 멈춰선 백조 (내일 이동 시도)
-visited = [[False] * m for _ in range(n)]
-swan_q.append(swans[0])
-visited[swans[0][0]][swans[0][1]] = True
+# 입력
+n, m = map(int, input().split())
+grid = [list(input().strip()) for _ in range(n)]
 
 
-def melt():
-    while water_q:
-        x, y = water_q.popleft()
-        for d in range(4):
-            nx, ny = x + dx[d], y + dy[d]
-            if 0 <= nx < n and 0 <= ny < m:
-                if grid[nx][ny] == 'X':
-                    grid[nx][ny] = '.' # 바꿔줘야 백조가 다음날 이동이 가능
-                    next_water_q.append((nx, ny))
-
-
-def move_swan():
+# 빙하 녹음
+# 백조 이동 시도
+# 시간 출력
+def move_swans():
     while swan_q:
         x, y = swan_q.popleft()
-        if x == swans[1][0] and y == swans[1][1]:
+
+        if grid[x][y] == 'L' and x == swans[1][0] and y == swans[1][1]:
             return True
+
         for d in range(4):
             nx, ny = x + dx[d], y + dy[d]
             if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
@@ -56,11 +27,46 @@ def move_swan():
                     next_swan_q.append((nx, ny))
     return False
 
+
+def melt_ice():
+    while water_q:
+        x, y = water_q.popleft()
+        for d in range(4):
+            nx, ny = x + dx[d], y + dy[d]
+            if 0 <= nx < n and 0 <= ny < m:
+                if grid[nx][ny] == 'X':
+                    grid[nx][ny] = '.'
+                    next_water_q.append((nx, ny))
+
+
+from collections import deque
+
+swans = []
+water_q = deque()
+next_water_q = deque()
+visited = [[False] * m for _ in range(n)]
+
+swan_q = deque()
+next_swan_q = deque()
+
+for i in range(n):
+    for j in range(m):
+        if grid[i][j] != 'X':  # 물 or 백조
+            water_q.append((i, j))
+        if grid[i][j] == 'L':
+            swans.append((i, j))
+
+swan_q.append(swans[0])  # x,y
+visited[swans[0][0]][swans[0][1]] = True
+
+day = 0
 while True:
-    if move_swan():
-        print(days)
+    if move_swans():
+        print(day)
         break
-    melt()
-    swan_q, next_swan_q = next_swan_q, deque()
-    water_q, next_water_q = next_water_q, deque()
-    days += 1
+    melt_ice()
+    water_q = next_water_q
+    swan_q = next_swan_q
+    next_water_q = deque()
+    next_swan_q = deque()
+    day += 1
