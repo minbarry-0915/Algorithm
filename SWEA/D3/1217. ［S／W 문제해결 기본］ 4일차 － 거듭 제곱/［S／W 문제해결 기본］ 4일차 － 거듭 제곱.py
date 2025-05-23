8a1261bbf7266118c17168b229e9a6966cee9419
LEE JIMIN <80612109+minbarry-0915@users.noreply.github.com>
2025-05-23 15:18:30 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 63 ms, Memory: 50,048 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.py
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.py
@@ -1,11 +1,11 @@
-def exponential(n,m):
-    if m == 0:
+def get_exp(num, exp):
+    if exp == 0:
         return 1
-    return n * exponential(n, m - 1)
+    return num * get_exp(num, exp - 1)
+
 
 for _ in range(10):
     t = int(input())
-    n,m = map(int, input().split())
-
-    answer = exponential(n,m)
-    print(f'#{t} {answer}')
+    num, exp = map(int, input().split())
+    result = get_exp(num, exp)
+    print(f'#{t} {result}')
