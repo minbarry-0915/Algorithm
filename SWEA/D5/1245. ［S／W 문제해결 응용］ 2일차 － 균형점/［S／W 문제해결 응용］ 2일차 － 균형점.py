7cc518eeae3762de309506cb707f84ce18fe58ca
LEE JIMIN <80612109+minbarry-0915@users.noreply.github.com>
2025-05-23 16:25:37 +0900

    [D5] Title: [S/W 문제해결 응용] 2일차 - 균형점, Time: 72 ms, Memory: 59,136 KB -BaekjoonHub

--- SWEA/D5/1245. ［S／W 문제해결 응용］ 2일차 － 균형점/［S／W 문제해결 응용］ 2일차 － 균형점.py
+++ SWEA/D5/1245. ［S／W 문제해결 응용］ 2일차 － 균형점/［S／W 문제해결 응용］ 2일차 － 균형점.py
@@ -2,29 +2,32 @@ T = int(input())
 for t in range(1, T + 1):
     n = int(input())
     temp = list(map(int, input().split()))
-    x = temp[:n] # 좌표
-    m = temp[n:] # 질량
-    
-    # 전략
-    # 두개씩 선택해서 좌우 인력이 일치하는 중간 지점을 탐색
-    result = []
+    locs = temp[:n]
+    mass = temp[n:]
+    results = []
+
     for i in range(n - 1):
-        left = x[i]
-        right = x[i + 1]
-        while right - left >= 1e-12:
+        left = locs[i]
+        right = locs[i + 1]
+
+        while right - left > 1e-12:
             mid = (left + right) / 2
-            left_force = right_force = 0
-            for i in range(n):
-                force = m[i] / (mid - x[i]) ** 2
-                if x[i] < mid: # 왼쪽 힘이면
-                    left_force += force
+            left_f = 0
+            right_f = 0
+
+            for j in range(n):
+                if locs[j] < mid:
+                    left_f += mass[j] / ((mid - locs[j]) ** 2)
                 else:
-                    right_force += force
-            if left_force < right_force: # 오른쪽힘이 더 세면 -> 오른쪽 기준점 낮추기
-                right = mid
-            else: # 왼쪽힘이 더 세면
+                    right_f += mass[j] / ((locs[j] - mid) ** 2)
+
+            if left_f > right_f:
                 left = mid
-        result.append(mid)
+            else:
+                right = mid
+        results.append(mid)
 
     print(f'#{t}', end=' ')
-    print(' '.join('%.10f' %f for f in result))
+    for r in results:
+        print(f'{r:.10f}', end=' ')
+    print()
