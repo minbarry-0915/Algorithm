2aa340f5ff2a68ec29841dde0e8652786b923896
LEE JIMIN <80612109+minbarry-0915@users.noreply.github.com>
2025-05-23 15:00:03 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문2, Time: 2,659 ms, Memory: 75,008 KB -BaekjoonHub

--- SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
+++ SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
@@ -1,24 +1,19 @@
-def is_palindrome(target):
-    return target[::] == target[::-1]
-
 for _ in range(10):
     t = int(input())
-    grid = [input().strip() for _ in range(100)]
+    grid = [list(input().strip()) for _ in range(100)]
 
-    length = 1
-    max_len = -1
-    while length != 100:
+    max_len = 0
+    # 가로 체크
+    for n in range(1,101):
         for i in range(100):
-            for j in range(0, 100 - length + 1):
-                target = grid[i][j:j + length]
-                if is_palindrome(target):
-                    max_len = max(max_len, len(target))
-
+            for j in range(100 - n + 1):
+                window = grid[i][j:j + n]
+                if window[::] == window[::-1]:
+                    max_len = max(max_len, len(window))
+        # 세로 체크
         for j in range(100):
-            for i in range(0, 100 - length + 1):
-                target = ''.join([grid[k][j] for k in range(i, i + length)])
-                if is_palindrome(target):
-                    max_len = max(max_len, len(target))
-        length += 1
-
+            for i in range(100 - n + 1):
+                window = [grid[k][j] for k in range(i, i + n)]
+                if window[::] == window[:: -1]:
+                    max_len = max(max_len, len(window))
     print(f'#{t} {max_len}')
