d4556e40c057889fdf4203c7aa7dd5d6b6319ff5
LEE JIMIN <80612109+minbarry-0915@users.noreply.github.com>
2025-05-23 14:39:26 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - String, Time: 63 ms, Memory: 59,136 KB -BaekjoonHub

--- SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.py
+++ SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.py
@@ -1,12 +1,13 @@
 for _ in range(10):
     t = int(input())
-    token = input().strip()
-    target = input().strip()
+    token = list(input().strip())
+    sentence = list(input().strip())
+    len_token = len(token)
+    len_sentence = len(sentence)
+    count = 0
+    for i in range(0, len_sentence - len_token + 1):
+        window = sentence[i: i + len_token]
+        if token[::] == window[::]:
+            count += 1
+    print(f'#{t} {count}')
 
-    n = len(token)
-    answer = 0
-    for i in range(0, len(target) - n + 1):
-        window = target[i: i + n]
-        if window[::] == token[::]:
-            answer += 1
-    print(f'#{t} {answer}')
