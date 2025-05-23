eb656c880e60e3e2de213efbb5a83919b4ba9db6
LEE JIMIN <80612109+minbarry-0915@users.noreply.github.com>
2025-05-23 14:52:59 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문1, Time: 61 ms, Memory: 58,496 KB -BaekjoonHub

--- SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
+++ SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
@@ -1,23 +1,18 @@
-def is_palindrome(target):
-    return target[::] == target[::-1]
-
-
 for t in range(1, 11):
     n = int(input())
-    grid = [input().strip() for _ in range(8)]
-
+    grid = [list(input().strip()) for _ in range(8)]
     count = 0
-    # 가로 검사
+
+    # 가로 체크
     for i in range(8):
         for j in range(8 - n + 1):
-            target = grid[i][j: j + n]
-            if is_palindrome(target):
+            window = grid[i][j:j + n]
+            if window[::] == window[::-1]:
                 count += 1
-    # 세로 검사
+    # 세로 체크
     for j in range(8):
         for i in range(8 - n + 1):
-            target = ''.join([grid[k][j] for k in range(i, i + n)])
-            if is_palindrome(target):
+            window = [grid[k][j] for k in range(i, i + n)]
+            if window[::] == window[:: -1]:
                 count += 1
-
     print(f'#{t} {count}')
