3549d15a58352238e81274aa3b6989f22de5576c
LEE JIMIN <80612109+minbarry-0915@users.noreply.github.com>
2025-05-23 17:18:17 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 160 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.py
@@ -0,0 +1,9 @@
+n = int(input())
+p = [0] + list(map(int,input().split()))
+dp = [int(1e9)] * (n + 1)
+dp[0] = 0
+
+for i in range(1, n + 1):
+    for k in range(1, i + 1):
+        dp[i] = min(dp[i], dp[i - k] + p[k])
+print(dp[n])
