26c91c84b349276798e407e0a8dc2e437d161c8d
LEE JIMIN <80612109+minbarry-0915@users.noreply.github.com>
2025-05-28 22:14:29 +0900

    [Gold III] Title: 파일 합치기, Time: 1984 ms, Memory: 122176 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.py
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.py
@@ -1,26 +1,23 @@
 import sys
 
-# sys.stdin = open('input.txt', 'r')
+# sys.stdin = open('input.txt', 'r', encoding='utf-8')
 input = sys.stdin.readline
-
 T = int(input())
 for t in range(1, T + 1):
-    k = int(input())  # 파일 개수
-    lst = list(map(int, input().split()))
+    k = int(input())
+    files = list(map(int, input().split()))
 
     s_lst = [0] * (k + 1)
     for i in range(k):
-        s_lst[i + 1] = s_lst[i] + lst[i]
+        s_lst[i + 1] = s_lst[i] + files[i]
 
-    dp = [[0] * k for _ in range(k)]
+    dp = [[0] * (k + 1) for _ in range(k + 1)]
 
-    for l in range(2, k + 1):  # 구간 길이
-        for i in range(k - l + 1):  # 시작 인덱스
+    for l in range(2, k + 1):
+        for i in range(1, k - l + 2):
             j = i + l - 1
             dp[i][j] = float('inf')
-
-            for mid in range(i, j):  # 분할 지점
-                cost = dp[i][mid] + dp[mid + 1][j] + s_lst[j + 1] - s_lst[i]
+            for m in range(i,j):
+                cost = dp[i][m] + dp[m + 1][j] + s_lst[j] - s_lst[i - 1]
                 dp[i][j] = min(dp[i][j], cost)
-
-    print(dp[0][k - 1])
+    print(dp[1][k])
