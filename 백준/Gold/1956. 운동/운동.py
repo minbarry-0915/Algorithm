4f4e2a91934a78f295de59240a6822638b09d7ec
LEE JIMIN <80612109+minbarry-0915@users.noreply.github.com>
2025-06-15 16:59:32 +0900

    [Gold IV] Title: 운동, Time: 780 ms, Memory: 112224 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.py
+++ 백준/Gold/1956. 운동/운동.py
@@ -1,22 +1,20 @@
 import sys
+# sys.stdin = open('input.txt','r')
 
-# sys.stdin = open('input.txt', 'r', encoding='utf-8')
-input = sys.stdin.readline
-INF = int(1e9)
-# 접근: 플로이드 워셜
-v, e = map(int, input().split())
-graph = [[INF] * (v + 1) for _ in range(v + 1)]
-
+v,e = map(int,input().split())
+grid = [[int(1e9)] * (v + 1) for _ in range(v + 1)]
 for _ in range(e):
-    a, b, c = map(int, input().split())
-    graph[a][b] = c
+    a,b,cost = map(int,input().split())
+    grid[a][b] = cost
 
 for k in range(1, v + 1):
-    for a in range(1, v + 1):
-        for b in range(1, v + 1):
-            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+    for i in range(1, v + 1):
+        for j in range(1, v + 1):
+            if grid[i][j] > grid[i][k] + grid[k][j]: # 최적경로 발견
+                grid[i][j] = grid[i][k] + grid[k][j]
 
-result = INF
+ans = int(1e9)
 for i in range(1, v + 1):
-    result = min(result, graph[i][i])
-print(result if result != INF else -1)
+    ans = min(ans, grid[i][i])
+
+print(ans if ans != int(1e9) else -1)
