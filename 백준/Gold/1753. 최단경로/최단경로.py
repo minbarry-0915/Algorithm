a601579e764512f835512a3e67a3d75726371c1f
LEE JIMIN <80612109+minbarry-0915@users.noreply.github.com>
2025-07-01 16:21:56 +0900

    [Gold IV] Title: 최단경로, Time: 532 ms, Memory: 139504 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,34 +1,29 @@
-import sys
+v,e = map(int,input().split())
+k = int(input())
+
+graph = {i: [] for i in range(1,v+1)}
+for _ in range(e):
+    a,b,c = map(int,input().split())
+    graph[a].append((b,c))
+
 import heapq
 
-# sys.stdin = open('input.txt', 'r')  # 채점 시 주석
-input = sys.stdin.readline
-
-v, e = map(int, input().split())
-k = int(input())
-graph = {i: [] for i in range(1, v + 1)}
-
-for _ in range(e):
-    u, v_, cost = map(int, input().split())
-    graph[u].append((v_, cost))
-
-# 다익스트라
-INF = int(1e9)
-distance = [INF] * (v + 1)
-distance[k] = 0
-heap = [(0, k)]  # (비용, 정점)
-
+INF = float('inf')
+dijkstra = [INF] * (v + 1)
+dijkstra[k] = 0
+heap = []
+heapq.heappush(heap, (0,k))
 while heap:
-    dist, now = heapq.heappop(heap)
+    cur_cost, cur_node  = heapq.heappop(heap)
 
-    if distance[now] < dist:
+    if dijkstra[cur_node] < cur_cost:
         continue
 
-    for next_node, cost in graph[now]:
-        next_dist = dist + cost
-        if next_dist < distance[next_node]:
-            distance[next_node] = next_dist
-            heapq.heappush(heap, (next_dist, next_node))
+    for node,cost in graph[cur_node]:
+        new_cost = cur_cost + cost
+        if dijkstra[node] > new_cost:
+            dijkstra[node] = new_cost
+            heapq.heappush(heap, (new_cost, node))
 
 for i in range(1, v + 1):
-    print(distance[i] if distance[i] != INF else "INF")
+    print(dijkstra[i] if dijkstra[i] != INF else 'INF')
