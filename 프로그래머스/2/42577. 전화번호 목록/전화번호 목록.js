2f21ae9cc44f533bd65e1c2d51eb11a457e2ba96
LEE JIMIN <80612109+minbarry-0915@users.noreply.github.com>
2025-07-05 00:14:28 +0900

    [level 2] Title: 전화번호 목록, Time: 131.60 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,11 @@
+function solution(phone_book) {
+    phone_book.sort();  // 사전순 정렬
+
+    for (let i = 0; i < phone_book.length - 1; i++) {
+        if (phone_book[i + 1].startsWith(phone_book[i])) {
+            return false;
+        }
+    }
+
+    return true;
+}
