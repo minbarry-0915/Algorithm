34885c720892b9415dee2d179e873b082094938e
LEE JIMIN <80612109+minbarry-0915@users.noreply.github.com>
2025-07-25 12:32:23 +0900

    [Gold IV] Title: 이모티콘, Time: 132 ms, Memory: 120916 KB -BaekjoonHub

--- 백준/Gold/14226. 이모티콘/이모티콘.py
+++ 백준/Gold/14226. 이모티콘/이모티콘.py
@@ -1,11 +1,8 @@
+s = int(input())
 
 from collections import deque
-
-s = int(input())
-MAX = 1001
-
-visited = [[-1] * MAX for _ in range(MAX)]
 queue = deque()
+visited = [[-1] * 1001 for _ in range(1001)]
 
 queue.append((1,0))
 visited[1][0] = 0
@@ -17,14 +14,17 @@ while queue:
         print(visited[screen][clipboard])
         break
 
+    # 화면에 있는 이모티콘을 모두 복사해서 클립보드에 저장한다.
     if visited[screen][screen] == -1:
         visited[screen][screen] = visited[screen][clipboard] + 1
-        queue.append((screen,screen))
+        queue.append((screen, screen))
 
-    if clipboard > 0 and screen + clipboard < MAX and visited[screen + clipboard][clipboard] == -1:
+    # 클립보드에 있는 모든 이모티콘을 화면에 붙여넣기 한다.
+    if clipboard > 0 and screen + clipboard < 1001 and visited[screen + clipboard][clipboard] == -1:
         visited[screen + clipboard][clipboard] = visited[screen][clipboard] + 1
         queue.append((screen + clipboard, clipboard))
 
-    if screen - 1 >= 0 and visited[screen - 1][clipboard] == -1:
+    # 화면에 있는 이모티콘 중 하나를 삭제한다.
+    if screen > 0 and visited[screen - 1][clipboard] == -1:
         visited[screen - 1][clipboard] = visited[screen][clipboard] + 1
         queue.append((screen - 1, clipboard))
