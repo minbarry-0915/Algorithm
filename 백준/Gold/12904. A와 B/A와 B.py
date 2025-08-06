187c23011f29fa7c06fa4dde3faedb4ef37fce3d
LEE JIMIN <80612109+minbarry-0915@users.noreply.github.com>
2025-08-06 17:16:30 +0900

    [Gold V] Title: A와 B, Time: 100 ms, Memory: 109416 KB -BaekjoonHub

---
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -0,0 +1,14 @@
+s = list(input().strip())
+t = list(input().strip())
+
+while len(t) > len(s):
+  if t[-1] == 'A':
+    t = t[:-1]
+  elif t[-1] == 'B':
+    t = t[:-1]
+    t = t[::-1]
+
+if t == s:
+  print(1)
+else:
+  print(0)
