62e7636924268f35b718ccf9082b90275f991e2d
LEE JIMIN <80612109+minbarry-0915@users.noreply.github.com>
2025-09-01 15:51:05 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 348 ms, Memory: 140424 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,17 +1,16 @@
-import sys
-input = sys.stdin.readline
+n,m = map(int,input().split())
+grid = [list(map(int,input().split())) for _ in range(n)]
+queries = [list(map(int,input().split())) for _ in range(m)]
 
-n, m = map(int, input().split())
+# 1-based indexing으로 안전하게 만들기
+prefix_sum = [[0] * (n + 1) for _ in range(n + 1)]
 
-arr = [list(map(int, input().split())) for _ in range(n)]
-
-dp = [[0] * (n + 1) for _ in range(n + 1)]
-
+# 1. 누적합 계산
 for i in range(1, n + 1):
-  for j in range(1, n + 1):
-    dp[i][j] = dp[i][j - 1] + dp[i - 1][j] - dp[i - 1][j - 1] + arr[i -1][j - 1] #arr 는 0부터 저장
+    for j in range(1, n + 1):
+        prefix_sum[i][j] = grid[i-1][j-1] + prefix_sum[i-1][j] + prefix_sum[i][j-1] - prefix_sum[i-1][j-1]
 
-for _ in range(m):
-  x1,y1,x2,y2 = map(int, input().split())
-  result = dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1]
-  print(result)
+# 2. 쿼리 결과 계산
+for x1, y1, x2, y2 in queries:
+    result = prefix_sum[x2][y2] - prefix_sum[x1-1][y2] - prefix_sum[x2][y1-1] + prefix_sum[x1-1][y1-1]
+    print(result)  
