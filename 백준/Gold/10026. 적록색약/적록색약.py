6f651774dcf82f491f6f873d1fe1754cdd731c74
LEE JIMIN <80612109+minbarry-0915@users.noreply.github.com>
2025-09-20 01:16:19 +0900

    [Gold V] Title: 적록색약, Time: 152 ms, Memory: 115536 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,48 +1,43 @@
-from collections import deque
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-
-def bfs(i, j, target, visited, grid):
-    queue = deque()
-    queue.append((i, j))
-    visited[i][j] = 1
-
-    while queue:
-        cx, cy = queue.popleft()
-        for d in range(4):
-            nx, ny = cx + dx[d], cy + dy[d]
-            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny] and grid[nx][ny] == target:
-                visited[nx][ny] = 1
-                queue.append((nx, ny))
-
-
 n = int(input())
-grid_normal = [list(input()) for _ in range(n)]
+grid = [list(input().strip()) for _ in range(n)]
 
-# 일반 부분 탐색
-normal_cnt = 0
-visited_normal = [[0] * n for _ in range(n)]
+visited1 = [[False] * n for _ in range(n)]
+visited2 = [[False] * n for _ in range(n)]
+
+count1 = 0
+count2 = 0
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+from collections import deque
+def bfs(x,y,initialcolor,visited,isblind):
+    queue = deque()
+    visited[x][y] = True
+    queue.append((x,y))
+    while queue:
+        cx,cy = queue.popleft()
+
+        for d in range(4):
+            nx = cx + dx[d]
+            ny = cy + dy[d]
+
+            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
+                if isblind:
+                    if (initialcolor == 'R' and grid[nx][ny] == 'G') or (initialcolor == 'G' and grid[nx][ny] == 'R') or (initialcolor == grid[nx][ny]):
+                        visited[nx][ny] = True
+                        queue.append((nx,ny))
+                else:
+                    if initialcolor == grid[nx][ny]:
+                        visited[nx][ny] = True
+                        queue.append((nx,ny))
+    return 1
+
 for i in range(n):
     for j in range(n):
-        if not visited_normal[i][j]:
-            bfs(i, j, grid_normal[i][j], visited_normal, grid_normal)
-            normal_cnt += 1
+        if not visited1[i][j]:
+            count1 += bfs(i,j,grid[i][j],visited1,False)
+        if not visited2[i][j]:
+            count2 += bfs(i,j,grid[i][j],visited2,True)
 
-# 적록 색약 버전으로 변경
-grid_for_blind = [row[:] for row in grid_normal]
-for i in range(n):
-    for j in range(n):
-        if grid_for_blind[i][j] == 'G':
-            grid_for_blind[i][j] = 'R'
-
-# 적록 색약 탐색
-visited_blind = [[0] * n for _ in range(n)]
-blind_cnt = 0
-for i in range(n):
-    for j in range(n):
-        if not visited_blind[i][j]:
-            bfs(i, j, grid_for_blind[i][j], visited_blind, grid_for_blind)
-            blind_cnt += 1
-print(normal_cnt, blind_cnt)
+print(count1, count2)
