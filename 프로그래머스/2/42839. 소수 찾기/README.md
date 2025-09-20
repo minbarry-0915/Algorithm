a0e9a79893b50fbf967dc67c6f93648469fa3dd0
LEE JIMIN <80612109+minbarry-0915@users.noreply.github.com>
2025-09-20 15:42:40 +0900

    [level 2] Title: 소수 찾기, Time: 29.52 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=java) 
 
 ### 성능 요약
 
-메모리: 9.44 MB, 시간: 10.34 ms
+메모리: 85 MB, 시간: 29.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 20일 15:28:00
+2025년 09월 20일 15:42:38
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -4,36 +4,39 @@ class Solution {
     Set<Integer> numbers = new HashSet<>();
     
     public int solution(String numbersStr) {
-        boolean[] visited = new boolean[numbersStr.length()];
-        dfs("", numbersStr, visited);
+        // dfs 돌려서 경우의 수 set 저장
+        // 소수인것만 골라서 계산
+        int n = numbersStr.length();
+        boolean[] visited = new boolean[n];
+        dfs("", visited, numbersStr, n);
+        int result = 0;
         
-        int count = 0;
         for (int num: numbers){
-            if(isPrime(num)) count ++;
+            if (isPrime(num)) result ++;
         }
-        
-        return count;
+        return result;
     }
     
-    void dfs(String current, String numbersStr, boolean[] visited){
-        if (!current.equals("")){
+    void dfs(String current, boolean[] visited, String numbersStr, int n){
+        if (current != ""){
             numbers.add(Integer.parseInt(current));
         }
         
-        for (int i = 0; i < numbersStr.length(); i ++){
+        for (int i = 0; i < n; i++){
             if (!visited[i]){
                 visited[i] = true;
-                dfs(current + numbersStr.charAt(i), numbersStr, visited);
+                dfs(current + numbersStr.charAt(i), visited, numbersStr, n);
                 visited[i] = false;
             }
         }
+        return;
     }
-    
-    boolean isPrime(int n){
-        if (n < 2) return false;
+
+    boolean isPrime(int x){
+        if (x < 2) return false;
         
-        for (int i = 2; i * i <= n; i++){
-            if (n % i == 0) return false;
+        for (int i = 2; i * i <= x; i ++){
+            if (x % i == 0) return false;
         }
         return true;
     }
