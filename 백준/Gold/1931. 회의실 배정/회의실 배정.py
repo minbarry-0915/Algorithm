6f71c6e5ac23db0c9441a61f2d0818c6c85da9f7
LEE JIMIN <80612109+minbarry-0915@users.noreply.github.com>
2025-10-15 16:20:48 +0900

    [Gold V] Title: 회의실 배정, Time: 352 ms, Memory: 126024 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -1,17 +1,12 @@
-N = int(input())
-schedules = []
-max_count = 0
-for _ in range(N):
-    start, end = map(int, input().split())
-    schedules.append((start, end))
+n = int(input())
+timetable = [tuple(map(int,input().split())) for _ in range(n)]
+timetable.sort(key= lambda x: (x[1],x[0]))
 
-# 종료시간 기준 정렬, 같은 종료시간일 경우 시작시간 기준
-schedules.sort(key = lambda x: (x[1],x[0]))
 current_end_time = 0
+cnt = 0
 
-for start, end in schedules:
+for start, end in timetable:
     if start >= current_end_time:
-        max_count += 1
+        cnt += 1
         current_end_time = end
-
-print(max_count)
+print(cnt)
